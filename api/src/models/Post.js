85075071d604d9414661056009891d320ca51a94
luistaffelli <luistaffelli@gmail.com>
2021-10-23 16:57:47 -0300

    Api first commit with databse

---
+++ api/src/models/Post.js
@@ -0,0 +1,16 @@
+const { DataTypes, Sequelize } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('post',{
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
+};
