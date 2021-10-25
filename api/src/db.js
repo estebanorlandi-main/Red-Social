574edb59db738072b76f8c04ec67b89e3eec4177
luistaffelli <luistaffelli@gmail.com>
2021-10-25 14:05:33 -0300

    Privilegios y relaciones

--- api/src/db.js
+++ api/src/db.js
@@ -6,11 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-<<<<<<< HEAD
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/redsocial`, {
-=======
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/red-social`, {
->>>>>>> 7c2c72b047947349230a54e129aeba59311d860d
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -31,13 +27,32 @@ let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].s
 sequelize.models = Object.fromEntries(capsEntries);
 
 
-const { User, Post, Comment } = sequelize.models;
+const { User, 
+  Post, 
+  Comment, 
+  Privileges, 
+  Post_User,
+  Comment_Post,
+  User_Comment } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Relacion 1 a M
-// User -> Post
-User.hasMany(Post)
-Post.belongsTo(User)
+//Usuario
+// Relacion 1 a M  - User -> Post
+User.belongsToMany(Post, {through: Post_User})
+Post.belongsTo(User, {through: Post_User})
+
+//Relacion 1 a 1 - User -> Privileges
+User.hasOne(Privileges)
+
+//Relacion 1 a M - User -> Comment
+User.belongsToMany(Comment, {through: User_Comment})
+Comment.belongsTo(User, {through: User_Comment})
+
+//Post
+//Relacion M a 1 - Post -> Comment
+Post.belongsToMany(Comment, {through: Comment_Post})
+Comment.belongsTo(Post, {through: Comment_Post})
+
 
 
 module.exports = {
