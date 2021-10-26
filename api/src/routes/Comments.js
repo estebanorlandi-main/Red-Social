f14a6581d364a8d6ee94192b722322b8e1788686
santiagoariel98 <santiagovillanuevaariel@gmail.com>
2021-10-26 10:36:51 -0300

    fixed: DB-Models

--- api/src/routes/Comments.js
+++ api/src/routes/Comments.js
@@ -1,31 +1,14 @@
 const router = require('express').Router();
 const {User,Post,Comment,User_Comment,Comment_Post,Post_User} = require('../db.js');
 
-async function DB_comments (){
-
-	const Comments_data = await Comments.findAll({
-		include: {
-			attributes: ['userID','title'],
-			through: {
-				attributes: [],
-			}
-		}
-	})
-	const final = Comments_data.map((Comment)=>{
-		return Comment.dataValues;
-	})
-	return final;
-
-}
-
 router.get('/:username', (req, res)=>{
 	res.send('hola, get')
 })
 
-router.post('/', (req, res)=>{
+router.post('/', async (req, res)=>{
 	try {
 		const{title,content} = req.body
-		Comment = await Comments.create({title,content})
+		const Comment = await Comments.create({title,content})
 		res.send(Comment)
 	} catch(e) {
 		res.sendStatus(404)
--- api/src/routes/Users.js
+++ api/src/routes/Users.js
@@ -2,19 +2,10 @@ const router = require('express').Router();
 const { Sequelize, Model } = require("sequelize");
 const {User,Post,Comment,User_Comment,Comment_Post,Post_User} = require('../db.js');
 const Op = Sequelize.Op;
-
-async function DB_UserID (username){
-	const UserID = await User.findOne({
-			where:{
-				username
-			},
-			include: [Post]
-		})
-	return UserID;
-}
-
+const {DB_UserID,validateUpdateUser} = require("./utils.js")
 
 router.get("/", async (req,res,next)=>{
+	if(req.query.q) return next()
 	try {
 		const users = await User.findAll({})
 		res.send(users)
@@ -22,27 +13,61 @@ router.get("/", async (req,res,next)=>{
 		res.sendStatus(404)
 	}
 })
-router.get("/:username", async (req,res,next)=>{
+
+//user query
+router.get("/", async (req,res,next)=>{
+	const query = req.query.q
 	try {
-		const {username} = req.params
-		const userID = await DB_UserID(username)
-		userID?res.send(userID):res.send(404)
+		const users = await User.findAll({
+			where:{
+				[Op.or]:{
+					name: query				
+				},
+				[Op.or]:{
+					lastname: query	
+				},
+				[Op.or]:{
+					username: query
+				}
+			}
+		})
+		res.send(users)
 	} catch(e) {
 		res.sendStatus(404)
 	}
 })
-router.get("/:username/posts", async (req,res,next)=>{
+//user params
+router.get("/:username", async (req,res,next)=>{
+	const {username} = req.params
 	try {
-		const {username} = req.params
 		const userID = await DB_UserID(username)
-		userID?res.send(userID.posts):res.send(404)
+		userID?res.send(userID):res.sendStatus(404)
+	} catch(e) {
+		res.sendStatus(404)
+	}
+})
+//user params post
+router.get("/:username/posts", async (req,res,next)=>{
+	const {username} = req.params
+	try {
+		const userID = await DB_UserID(username)
+		userID?res.send(userID.posts):res.sendStatus(404)
+	} catch(e) {
+		res.sendStatus(404)
+	}
+})
+router.get("/:username/comments", async(req,res,next)=>{
+	const {username} = req.params
+	try {
+		const userID = await DB_UserID(username)
+		userID?res.send(userID.comments):res.sendStatus(404)
 	} catch(e) {
 		res.sendStatus(404)
 	}
 })
 router.post("/register", async (req,res,next)=>{
+	const {name,lastname,username, password,mail,gitaccount,image} = req.body
 	try {
-		const {name,lastname,username, password,mail,gitaccount,image} = req.body
 		const user = await User.findOrCreate({
 			where:{
 				mail:mail
@@ -57,9 +82,24 @@ router.post("/register", async (req,res,next)=>{
 				image
 			}
 		})
-		res.send(200)
+		res.sendStatus(200)
 	} catch(e) {
 		res.sendStatus(400)
 	}
 })
+router.put("/:username", async (req,res,next)=>{
+	const {username} = req.params
+	try {
+		let userID = await DB_UserID(username)
+		const updateDates = validateUpdateUser(req.body,userID)
+		for(prop in updateDates){
+			userID[prop] = updateDates[prop]
+		}
+		await userID.save()
+		res.send(userID)
+	} catch(e) {
+		res.sendStatus(400)
+	}
+})
+
 module.exports = router;
