71ef4e36261a3c143ba72cbe495df8d978464388
LavalAlex <lavalalexander@gmail.com>
2021-10-28 14:50:58 -0300

    feat-Post/DB_post

--- api/index.js
+++ api/index.js
@@ -4,14 +4,17 @@ const { conn } = require('./src/db.js');
 
 // PARA CARGAR DATOS
 const DataUsers = require("../Mockups/DB_USERS.json")
+const dataPosts = require("../Mockups/DB_Posts.json")
 // const {User} = require('./src/db.js');
-const {DB_userCreates} = require("./src/routes/utils.js")
+const {DB_userCreates, DB_postCreates} = require("./src/routes/utils.js")
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     //CARGA DE DATOS
-    /*DB_userCreates(DataUsers)*/
+    // DB_userCreates(DataUsers)
+    //Descomentar esta linea para cargar los post
+    // DB_postCreates(dataPosts)
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
