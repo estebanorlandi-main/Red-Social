e12da086cd7c5ef595139f4cbfb43ed8bdd3c54a
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-10-28 11:29:54 -0300

    fix: actions

--- client/src/Redux/actions/Post.js
+++ client/src/Redux/actions/Post.js
@@ -1,18 +1,30 @@
-export const LIKE = "LIKE";
-export const DELETE = "LIKE";
-export const UPDATE = "LIKE";
+export const LIKE_POST = "LIKE_POST";
+export const SHARE_POST = "SHARE_POST";
+export const COMMENT_POST = "COMMENT_POST";
+export const DELETE_POST = "DELETE_POST";
+export const UPDATE_POST = "UPDATE_POST";
 
 export function likePost(postId) {
   console.log(postId);
-  return { type: LIKE, payload: postId };
+  return { type: LIKE_POST, payload: postId };
+}
+
+export function sharePost(postId) {
+  console.log(postId);
+  return { type: SHARE_POST, payload: postId };
+}
+
+export function commentPost(postId) {
+  console.log(postId);
+  return { type: COMMENT_POST, payload: postId };
 }
 
 export function deletePost(postId) {
   console.log(postId);
-  return { type: LIKE, payload: postId };
+  return { type: DELETE_POST, payload: postId };
 }
 
 export function updatePost(postId, data) {
   console.log(postId, data);
-  return { type: UPDATE, payload: { postId } };
+  return { type: UPDATE_POST, payload: { postId } };
 }
