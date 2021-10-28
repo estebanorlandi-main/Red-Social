1538e6b4b728cfed4188221a98b2486a2a94034c
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-10-28 11:38:51 -0300

    fix: App.js conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,8 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
-<<<<<<< HEAD
 import Signup from "./components/Signup/Signup";
-=======
-import Post from "./components/Post/Post";
 
->>>>>>> 0835a59c06d375ade85fce3b15e78cf30901c24c
 function App() {
   return (
     <BrowserRouter>
@@ -21,7 +17,7 @@ function App() {
               return <div></div>;
             }}
           />
-          <Route path="/signup" component={Signup}/>
+          <Route path="/signup" component={Signup} />
         </Switch>
       </div>
     </BrowserRouter>
