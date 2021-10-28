c418a8eaed4c1d7e508601e9909efe18396e3780
Andres <andrescimma1@gmail.com>
2021-10-28 00:31:48 -0300

    feature<Profile>: data displayed and can be modified

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
-import Profile from "./Pages/Profile/Profile";
+import Profile from "./Pages/Profile/Profile.jsx";
 
 function App() {
   return (
