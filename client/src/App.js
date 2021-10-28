e5a591433fbbd00ff8115e2da5e50d9dabdf47e3
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-10-28 11:31:37 -0300

    fix: removed selector from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
 import Post from "./components/Post/Post";
-import { useSelector } from "react-redux";
 
 function App() {
-  const posts = useSelector((state) => state.posts);
   return (
     <BrowserRouter>
       <div className="App">
@@ -16,13 +14,7 @@ function App() {
           <Route
             path="/test"
             render={() => {
-              return (
-                <div>
-                  <Post post={{ ...posts[0], image: undefined }} />
-                  <Post post={posts[1]} />
-                  <Post post={posts[2]} />
-                </div>
-              );
+              return <div></div>;
             }}
           />
         </Switch>
