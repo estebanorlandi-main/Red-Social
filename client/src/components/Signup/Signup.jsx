5009960d77bdf76d4b991da170fa687ec19d61f6
mateo0697 <mateopaluci@gmail.com>
2021-10-29 16:55:26 -0300

    colgue en cambiar rama y cambie singup y userlist

--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -24,54 +24,55 @@ function Signup(props) {
 
   function validarUsername(str){
     if (inputs.username.length < 3) {
-      return 5
+      return {num:5, bol:false}
     }
     if (inputs.username.length > 16) {
-      return 6
+      return {num:6, bol:false}
     }
-    if (tieneLet(str)) {
-      return 3
+    if (!tieneLet(str)) {
+      return {num:3, bol:false}
     }
-    if (tieneNum(str)) {
-      return 4
+    if (!tieneNum(str)) {
+      return {num:4, bol:false}
     }
     if (tieneSimb(str)) {
-      return 2
+      return {num:2, bol:false}
     }
   //  if () {
   //    return 7
   //  }
-    return true
+    return {num:0,bol:true }
   }
 
   function validarPassword(str){
 
     if (inputs.password.length < 8) {
-      return 5
+      return {num:5, bol:false}
     }
     if (inputs.password.length > 16) {
-      return 6
+      return {num:6, bol:false}
     }
-    if (tieneLet(str)) {
-      return 3
+    if (!tieneLet(str)) {
+      return {num:3, bol:false}
     }
-    if (tieneNum(str)) {
-      return 4
+    if (!tieneNum(str)) {
+      return {num:4, bol:false}
     }
     if (tieneSimb(str)) {
-      return 2
+      return {num:2, bol:false}
     }
-    return true
+    return {num:0, bol:true}
   }
 
   function validarNombreyApellido(str){
     if (tieneNum(str) || tieneSimb(str)) {
-      return 2
-    }
+      return {num:2, bol:false}
+    }else return {num:0, bol:true}
   }
 
   function validar(){
     var arr = [];
+    let validar = [];
     for (var propiedad in inputs) {
       if (!inputs[propiedad].length) {
         arr.push(propiedad)
@@ -79,11 +80,11 @@ function Signup(props) {
     }
 
     if (arr.length) {
-      console.log(err[arr[0]].indice)
       arr.forEach((i) => {
         if (i==="about" || i==="tags") {
           return
         }
+        validar.push(false)
         setErr(obj=>{
           return {...obj, [i]:{...obj[i], indice:1}}
         });
@@ -91,49 +92,59 @@ function Signup(props) {
   }
 
   if (!arr.includes("username")) {
-    console.log()
+    let indice = validarUsername("username")
+    validar.push(indice.bol)
     setErr((old) => ({
       ...old,
-      username:{...old.username, indice:validarUsername("username")}
+      username:{...old.username, indice:indice.num}
     }))
   }
 
   if (!arr.includes("password")) {
+    let indice = validarPassword("password")
+    validar.push(indice.bol)
     setErr((old) => ({
       ...old,
-      password:{...old.password, indice:validarPassword("password")}
+      password:{...old.password, indice: indice.num}
     }))
   }
 
   if (!arr.includes("name")) {
+    let indice = validarNombreyApellido("name")
+    validar.push(indice.bol)
       setErr((old) => ({
         ...old,
-        name:{...old.name, indice:validarNombreyApellido("name")}
+        name:{...old.name, indice: indice.num}
       }))
   }
   if (!arr.includes("lastName")) {
+    let indice = validarNombreyApellido("lastName")
+    validar.push(indice.bol)
     setErr((old) => ({
       ...old,
-      lastName:{...old.lastName, indice:validarNombreyApellido("lastName")}
+      lastName:{...old.lastName, indice: indice.num}
     }))
   }
+  return validar
 }
 
 function tieneNum(str){
   let regexnumeros = /([0-9]+)/
-  if (!regexnumeros.test(inputs[str])) {
+  if (regexnumeros.test(inputs[str])) {
     return true
   }
   return false
 }
+
 function tieneLet(str){
   let regexletrasmin = /([a-z]+)/
   let regexletrasmay = /([A-Z]+)/
-  if (!regexletrasmay.test(inputs[str]) && !regexletrasmin.test(inputs[str])) {
+  if (regexletrasmay.test(inputs[str]) || regexletrasmin.test(inputs[str])) {
     return true
   }
   return false
 }
+
 function tieneSimb(str){
   let regexFinal1 = /([\W])/
   let regexFinal2 = /[_]/
@@ -142,6 +153,7 @@ function tieneSimb(str){
   }
   return false
 }
+
   const handleChange = (e) => {
     setInputs((old) => ({
       ...old,
@@ -155,54 +167,68 @@ function tieneSimb(str){
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    validar()
+    let validacion = validar()
+    if (!validacion.includes(false)) {
+      console.log("aca se haria la llamada a la action")
+      setInputs(
+        {
+          username: "",
+          password: "",
+          name: "",
+          lastName: "",
+          email: "",
+          github: "",
+          about: "",
+          tags: "",
+        }
+      )
+    }
   };
-
   return (
     <form className={style.container} onSubmit={(e)=>handleSubmit(e)} onChange={(e)=>handleChange(e)}>
       Formulario de registro
       <label>
         Username
       </label>
-        <input className={style.input}  name="username" type="text" />
-        <div>{err.username.errores[err.username.indice]}</div>
+        <input className={style.input}  value={inputs.username}name="username" type="text" />
+        <div className={style.errores}>{err.username.errores[err.username.indice]}</div>
       <label>
         Password
       </label>
-        <input className={style.input} name="password" type="password" />
-        <div>{err.password.errores[err.password.indice]}</div>
+        <input className={style.input} value={inputs.password}name="password" type="password" />
+        <div className={style.errores}>{err.password.errores[err.password.indice]}</div>
       <label>
         Name
       </label>
-        <input className={style.input} name="name" type="text" />
-        <div>{err.name.errores[err.name.indice]}</div>
+        <input className={style.input} value={inputs.name}name="name" type="text" />
+        <div className={style.errores}>{err.name.errores[err.name.indice]}</div>
       <label>
         Last Name
       </label>
-        <input className={style.input} name="lastName" type="text" />
-        <div>{err.lastName.errores[err.lastName.indice]}</div>
+        <input className={style.input} value={inputs.lastName}name="lastName" type="text" />
+        <div className={style.errores}>{err.lastName.errores[err.lastName.indice]}</div>
       <label>
         Email
       </label>
-        <input className={style.input} name="email" type="email" />
-        <div>{err.email.errores[err.email.indice]}</div>
+        <input className={style.input} value={inputs.email}name="email" type="email" />
+        <div className={style.errores}>{err.email.errores[err.email.indice]}</div>
       <label>
         GitHub
       </label>
-        <input className={style.input} name="github" type="text" />
-        <div>{err.github.errores[err.github.indice]}</div>
+        <input className={style.input} value={inputs.github}name="github" type="text" />
+        <div className={style.errores}>{err.github.errores[err.github.indice]}</div>
       <label>
         Avatar
       </label>
-        <input className={style.input} name="avatar" type="image" />
+        <input className={style.input} value={inputs.avatar}name="avatar" type="image" />
       <label>
         About
       </label>
-        <textarea className={style.input} name="about" type="text"></textarea>
+        <textarea className={style.input} value={inputs.about}name="about" type="text"></textarea>
       <label>
         Tags
       </label>
-        <input className={style.input} name="tags" type="text" />
+        <input className={style.input} value={inputs.tags}name="tags" type="text" />
         <button type="submit">Crear</button>
     </form>
   );
