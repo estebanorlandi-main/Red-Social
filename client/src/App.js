ab18680db219d5e5424c60324a616c0db5b37ff2
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-10-30 15:01:53 -0300

    fix: navbar

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,25 @@ import Signup from "./components/Signup/Signup";
 import UserList from "./components/UserList/UserList";
 import NewPost from "./components/NewPost/NewPost";
 import NavBar from "./components/NavBar/NavBar.js";
-import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <NavBar />
+
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route path="/home" component={Home} />
           <Route path="/signup" component={Signup} />
-          <Route path="/profile" component={Profile} />
+          <Route
+            path="/profile/:username"
+            render={({
+              match: {
+                params: { username },
+              },
+            }) => <Profile user={username} />}
+          />
           <Route path="/newpost" component={NewPost} />
           <Route path="/userlist" component={UserList} />
 
