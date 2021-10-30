b942f6e02b20091e3cc449760a3ee5556382b1c5
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-10-30 14:32:59 -0300

    fix: profile route

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
 import Profile from "./Pages/Profile/Profile.jsx";
-import Post from "./components/Post/Post";
 import Signup from "./components/Signup/Signup";
-import Filters from "./components/Filters/Filters";
 
 function App() {
   return (
@@ -14,13 +12,15 @@ function App() {
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route path="/home" component={Home} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/profile" component={Profile} />
+
           <Route
             path="/test"
             render={() => {
               return <div></div>;
             }}
           />
-          <Route path="/signup" component={Signup} />
         </Switch>
       </div>
     </BrowserRouter>
