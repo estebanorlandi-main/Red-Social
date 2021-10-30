1f76dc9236bd3555b5836a2a06daa6ac5a49584a
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-10-30 18:28:32 -0300

    fix<App, index>: BrowserRoute changed to index

--- client/src/App.js
+++ client/src/App.js
@@ -17,47 +17,45 @@ function App() {
   const posts = useSelector((state) => state.posts);
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <NavBar />
+    <div className="App">
+      <NavBar />
 
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/home" component={Home} />
-          <Route path="/signup" component={Signup} />
-          <Route
-            path="/profile/:username"
-            render={({
-              match: {
-                params: { username },
-              },
-            }) => <Profile user={username} />}
-          />
-          <Route path="/newpost" component={NewPost} />
-          <Route path="/userlist" component={UserList} />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/home" component={Home} />
+        <Route path="/signup" component={Signup} />
+        <Route
+          path="/profile/:username"
+          render={({
+            match: {
+              params: { username },
+            },
+          }) => <Profile user={username} />}
+        />
+        <Route path="/newpost" component={NewPost} />
+        <Route path="/userlist" component={UserList} />
 
-          <Route
-            path="/test"
-            render={() => {
-              return (
-                <div>
-                  <Post post={posts[0]} />
-                  <Post post={posts[1]} />
-                  <Post post={posts[2]} />
-                  <Post post={posts[3]} />
-                  <Post post={posts[4]} />
-                  <Post post={posts[5]} />
-                  <Post post={posts[6]} />
-                  <Post post={posts[7]} />
-                  <Post post={posts[8]} />
-                  <Post post={posts[9]} />
-                </div>
-              );
-            }}
-          />
-        </Switch>
-      </div>
-    </BrowserRouter>
+        <Route
+          path="/test"
+          render={() => {
+            return (
+              <div>
+                <Post post={posts[0]} />
+                <Post post={posts[1]} />
+                <Post post={posts[2]} />
+                <Post post={posts[3]} />
+                <Post post={posts[4]} />
+                <Post post={posts[5]} />
+                <Post post={posts[6]} />
+                <Post post={posts[7]} />
+                <Post post={posts[8]} />
+                <Post post={posts[9]} />
+              </div>
+            );
+          }}
+        />
+      </Switch>
+    </div>
   );
 }
 
