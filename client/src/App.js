b98e0d1fa1a297d365ad39cc84d5d5557d77bb1b
mateo0697 <mateopaluci@gmail.com>
2021-11-01 17:35:39 -0300

    Redux de sesion, login, logout, register

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,48 @@ import LandingPage from "./Pages/LandingPage/LandingPage";
 import Home from "./Pages/Home/Home";
 import Profile from "./Pages/Profile/Profile.jsx";
 import Signup from "./components/Signup/Signup";
-
+import Login from "./components/Login/Login";
 import UserList from "./components/UserList/UserList";
 import NavBar from "./components/NavBar/NavBar.js";
-
+import {useEffect, useState} from "react"
+import {SingUp} from "./Redux/actions/Profile.js"
 // Variables CSS
 import "./App.css";
-
+import {useSelector, useDispatch} from 'react-redux'
+//--------------------
+//  Guarda la session
+// localStorage.setItem("CodeNet", JSON.stringify(action.payload));
+//
+//  Obtiene los datos de sesion
+// localStorage.getItem("CodeNet")
+//
+//  Elimina la sesion
+// localStorage.removeItem("CodeNet");
+//
+//--------------------
 function App() {
+  const usuario = useSelector(store=> store.sessionReducer)
+  const dispatch = useDispatch();
+  const [first, setFirst] = useState(true)
+  useEffect(()=>{
+    function verificar(){
+      if (usuario.username) {
+        console.log("entre")
+        localStorage.setItem("CodeNet", JSON.stringify(usuario));
+      }else {
+        localStorage.setItem("CodeNet", JSON.stringify({}));
+      }
+    }
+    verificar()
+  },[usuario])
+
+  // useEffect(()=>{
+  //   if (first) {
+  //     let localStorage = localStorage.getItem("CodeNet")
+  //     dispatch(SingUp(localStorage))
+  //     setFirst(false)
+  //   }
+  // })
   return (
     <div className="App">
       <NavBar />
@@ -19,6 +53,7 @@ function App() {
         <Route exact path="/" component={LandingPage} />
         <Route path="/home" component={Home} />
         <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login}/>
         <Route
           path="/profile/:username"
           render={({
--- client/src/Redux/reducer/session.js
+++ client/src/Redux/reducer/session.js
@@ -1,9 +1,10 @@
-const initialState = {};
+
+const initialState = JSON.parse(localStorage.getItem("CodeNet")) || {};
 
 export default function root(state = initialState, action) {
   switch (action.type) {
     case "LOG_IN":
-      return action.patload
+      return action.payload
     case "SING_UP":
       return action.payload
     case "LOG_OUT":
