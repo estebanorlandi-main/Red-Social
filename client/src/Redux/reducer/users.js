5da6b7a12ce03b33037c3ed6a439e57f05a2e69b
andrescimma1 <andrescimma1@gmail.com>
2021-11-01 16:15:37 -0300

    fix<SearchBar>: Conected to Redux

--- client/src/Redux/reducer/users.js
+++ client/src/Redux/reducer/users.js
@@ -1,11 +1,32 @@
 import users from "../../Mockups/users.json";
+import { FILTER_INPUT } from "../actions/SearchBar";
 
 const initialState = {
   users,
+  filteredUsers: [],
 };
 
 export default function root(state = initialState, action) {
   switch (action.type) {
+    case FILTER_INPUT:
+      if (action.input === "") {
+        return {
+          ...state,
+          filteredUsers: [],
+        };
+      } else {
+        return {
+          ...state,
+          filteredUsers: action.payload.filter((user) => {
+            if (
+              user.username.toLowerCase().includes(action.input.toLowerCase())
+            ) {
+              return user;
+            }
+          }),
+        };
+      }
+
     default:
       return state;
   }
