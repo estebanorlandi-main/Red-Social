8155915309722690d529b6a2b371bd16beecb4ae
LavalAlex <lavalalexander@gmail.com>
2021-11-06 19:05:13 -0300

    fix: ready reducer

--- client/src/Redux/reducer/support.js
+++ client/src/Redux/reducer/support.js
@@ -1,10 +1,10 @@
-import axios from "axios";
-import { ERROR } from "./Errors";
+// import axios from "axios";
+// import { ERROR } from "./Errors";
 
-export function createSupport(payload) {
-  return (dispatch) =>
-    axios
-      .post("http://localhost:3001/support", payload)
-      .then((res) => dispatch({ payload: res }))
-      .catch((err) => dispatch({ type: ERROR, payload: err }));
-}
+// export function createSupport(payload) {
+//   return (dispatch) =>
+//     axios
+//       .post("http://localhost:3001/support", payload)
+//       .then((res) => dispatch({ payload: res }))
+//       .catch((err) => dispatch({ type: ERROR, payload: err }));
+// }
