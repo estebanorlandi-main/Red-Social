9ab11d7262d94780759725cfd49c1292b3f2b5a1
LavalAlex <lavalalexander@gmail.com>
2021-11-06 23:39:08 -0300

    feat(front): reducer index admin

--- client/src/Redux/reducer/index.js
+++ client/src/Redux/reducer/index.js
@@ -10,9 +10,13 @@ import usersReducer from "./users.js";
 
 import errorsReducer from "./errors.js";
 
+import adminReducer from "./admin.js"
+
+
 export default combineReducers({
   postsReducer, // Array con todos los posteos / filtrar posteos - busqueda - ordernar
   sessionReducer, // Objeto Datos del usuario logeado / login - logout - register
   usersReducer, // Array con todos los usuarios / Filtrar usuarios - busqueda - ordenar
-  errorsReducer, // Contiene los errores de todos los reducers
+  errorsReducer,// Contiene los errores de todos los reducers
+  adminReducer //Objeto con todos los mesajes para el admin
 });
