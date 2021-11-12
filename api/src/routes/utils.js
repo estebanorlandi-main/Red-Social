b27d8c234d1830f251b24a8f448e62f00e40e9d1
LavalAlex <lavalalexander@gmail.com>
2021-11-12 14:40:04 -0300

    fix: bad db read

--- api/src/routes/utils.js
+++ api/src/routes/utils.js
@@ -414,12 +414,12 @@ const BD_createPrivileges = async (user) =>{
 
 
 const BD_searchAdmin = async (user) =>{
-  var privileges = await Privileges.findOne({where:{username:user.username}})
+  var privileges = await Privileges.findOne({where:{username:user.username}});
   return privileges
 }
 
 const BD_searchPost = async (idPost) =>{
-  var post = await Post.findOne({where:{idPost:idPost}})
+  var post = await Post.findOne({where:{idPost:idPost}});
   return post
 }
 
@@ -427,17 +427,19 @@ const BD_banUser = async (username) => {
   var user = await User.findOne({where:{username:username}});
   if(user === null) return {error:'User not exits'}
   if(user.strike === null){
-    user.strike = ['X']
-    user.save()
+    user.strike = ['X'];
+    var dayBan = new Date(Date.now() + 24 * 3600 * 1000);
+    user.dayBan = dayBan;
+    user.save();
     return {Succes: 'The STRIKE was applied successfully', Strike:user.strike.length}
   } else{
     if(user.strike.length === 1){
       user.strike = ['X','X'];
-      user.save()
+      user.save();
     }else{
       if(user.strike.length === 2){
         user.strike = ['X','X','X'];
-        user.save()
+        user.save();
       }
     }
     return {Succes: 'The STRIKE was applied successfully', Strike:user.strike.length}
@@ -448,7 +450,7 @@ const BD_loginBan = async (username) => {
   const user = await User.findOne({ where:{username: username}})
 
   if(user.strike.length === 3){
-    return {error: 'You are temporarily suspended'}
+    return {error: 'You are temporarily suspended'};
   }
   return {}
 }
